41f021264ba9725e76882da05eebb750c6182772
Mladen Čikara <mladen.cikara@gmail.com>
2014-05-06 00:51:46 +0200

    Sređen mock iPBX-a

---
+++ zeromq-ipbx-example/ipbx-mock/src/main/java/hr/podsjetnik/zeromq/ipbx/MessageProcessor.java
@@ -0,0 +1,15 @@
+package hr.podsjetnik.zeromq.ipbx;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+
+public class MessageProcessor implements Processor {
+
+	@Override
+	public void process(Exchange in) throws Exception {
+		String message = new String((byte[]) in.getIn().getBody());				
+		System.out.println(message);
+	}
+
+}
