f7a66e5cfd7168c32ee8f78cc1e5e1ccf027f0ac
Mladen Čikara <mladen.cikara@dentalvukic.com>
2014-05-10 10:39:25 +0200

    Sredio layout za login

--- zeromq-ipbx-example/vaadin-client/src/main/java/hr/podsjetnik/zeromq/vaadin/impl/LoginViewImpl.java
+++ zeromq-ipbx-example/vaadin-client/src/main/java/hr/podsjetnik/zeromq/vaadin/impl/LoginViewImpl.java
@@ -3,15 +3,18 @@ package hr.podsjetnik.zeromq.vaadin.impl;
 import hr.podsjetnik.zeromq.vaadin.LoginView;
 import hr.podsjetnik.zeromq.vaadin.LoginViewHandler;
 
+import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.VaadinSession;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-public class LoginViewImpl extends VerticalLayout implements LoginView {
+public class LoginViewImpl extends HorizontalLayout implements LoginView {
 
 	private static final long serialVersionUID = 5725359388678494023L;
 
@@ -29,13 +32,15 @@ public class LoginViewImpl extends VerticalLayout implements LoginView {
 
 	@Override
 	public void init() {
+		VerticalLayout layout = new VerticalLayout();
 		txtUsername = new TextField("Username:");
-		addComponent(txtUsername);
+		layout.addComponent(txtUsername);
+		layout.setComponentAlignment(txtUsername, Alignment.MIDDLE_CENTER);
 		txtPassword = new PasswordField("Password:");
-		addComponent(txtPassword);
+		layout.addComponent(txtPassword);
+		layout.setComponentAlignment(txtPassword, Alignment.MIDDLE_CENTER);
 
 		btnLogin = new Button("Login");
-		addComponent(btnLogin);
 		btnLogin.addClickListener(new Button.ClickListener() {
 
 			private static final long serialVersionUID = 582630689354182376L;
@@ -45,6 +50,15 @@ public class LoginViewImpl extends VerticalLayout implements LoginView {
 				handler.login();
 			}
 		});
+		btnLogin.setClickShortcut(KeyCode.ENTER);
+		layout.addComponent(btnLogin);		
+		layout.setComponentAlignment(btnLogin, Alignment.MIDDLE_CENTER);
+
+		addComponent(layout);
+		setComponentAlignment(layout, Alignment.MIDDLE_CENTER);
+		setSizeFull();
+		setSpacing(true);
+		setMargin(true);
 	}
 
 	@Override
